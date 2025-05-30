aa086725c5e8e7bfd7fc1c6f32514f80b255ad9d
Seungyun Lee <leeseun3@fit.cvut.cz>
2025-05-30 03:18:20 +0200

    chore: remove sample implementation

--- mila-compiler/main.cpp
+++ mila-compiler/main.cpp
@@ -1,14 +1,1 @@
-#include <parser/parser.h>
 
-int main (int argc, char *argv[])
-{
-    Parser parser;
-
-    if (!parser.Parse()) {
-        return 1;
-    }
-
-    parser.Generate().print(llvm::outs(), nullptr);
-
-    return 0;
-}
