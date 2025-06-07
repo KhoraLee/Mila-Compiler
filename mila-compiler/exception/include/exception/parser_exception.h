b99e060035dd8fb045ca005ded285a9668d50013
Seungyun Lee <leeseun3@fit.cvut.cz>
2025-06-07 23:28:04 +0200

    feat(exception): add parser exception classes

---
+++ mila-compiler/exception/include/exception/parser_exception.h
@@ -0,0 +1,26 @@
+#include <exception/exception.h>
+
+class ParserException : public MilaException {
+public:
+  using MilaException::MilaException;
+};
+
+class UnexpectedTokenException : public ParserException {
+public:
+  UnexpectedTokenException(SourceLocation loc, const std::string& message) : ParserException(loc, message) {}
+};
+
+class SyntaxErrorException : public ParserException {
+public:
+  SyntaxErrorException(SourceLocation loc, const std::string& message) : ParserException(loc, message) {}
+};
+
+class MissingTokenException : public ParserException {
+public:
+  MissingTokenException(SourceLocation loc, const std::string& message) : ParserException(loc, message) {}
+};
+
+class UnmatchedParenthesisException : public ParserException {
+public:
+  UnmatchedParenthesisException(SourceLocation loc, const std::string& message) : ParserException(loc, message) {}
+};
