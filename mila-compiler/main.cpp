17ddebfb1e20aa1c4a55bd731ae4bdfc52bf536e
Seungyun Lee <leeseun3@fit.cvut.cz>
2025-06-18 21:25:17 +0200

    feat: implement simple IR emitter from mila input

--- mila-compiler/main.cpp
+++ mila-compiler/main.cpp
@@ -1,1 +1,30 @@
+#include <parser/parser.h>
 
+#include <codegen/code_generator.h>
+
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* args[]) {
+  const char* fileName = args[1];
+  std::ifstream file;
+  file.open(fileName);
+
+  Lexer lexer(file);
+  Parser parser(lexer);
+  CodeGenerator codegen;
+  if (file.fail()) {
+    std::cerr << "Couldn't open file\n" << fileName << std::endl;
+    return 1;
+  } else {
+    try {
+      auto program = parser.parse();
+      program->accept(codegen);
+      
+      auto ir_module = codegen.getModule();
+      ir_module->print(llvm::outs(), nullptr);
+    } catch (MilaException e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+}
